c6fba355d56c42114d2d067e2b84783d6fcac927
Luca Berto <luca.berto@edu.unifi.it>
2024-11-29 13:24:16 +0100

    configurazione Googletest

---
+++ test/runAllTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by lucaberto on 29/11/24.
+//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
