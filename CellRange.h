f6c79c8d8f6ef900ed119c9108b80c09f2ea9030
Luca Berto <luca.berto@edu.unifi.it>
2024-12-05 00:50:58 +0100

    Aggiunta struttura CellRange

---
+++ CellRange.h
@@ -0,0 +1,39 @@
+//
+// Created by lucaberto on 05/12/24.
+//
+
+#ifndef CELLRANGE_H
+#define CELLRANGE_H
+
+#include <string>
+
+struct CellRange {
+    int startColumn;
+    int startRow;
+    int endColumn;
+    int endRow;
+};
+
+inline int columnToIndex(char column) {
+    return column - 'A';
+}
+
+inline CellRange convertRange(const std::string &range) {
+    CellRange result;
+
+    std::size_t found = range.find(":");
+    if (found != std::string::npos) {
+        std::string starCell = range.substr(0, found);
+        std::string endCell = range.substr(found + 1);
+        result.startColumn = columnToIndex(starCell[0]);
+        result.startRow = std::stoi(starCell.substr(1)) - 1;
+        result.endColumn = columnToIndex(endCell[0]);
+        result.endRow = std::stoi(endCell.substr(1)) - 1;
+    } else {
+        // TODO: lanciare eccezione
+    }
+
+    return result;
+}
+
+#endif //CELLRANGE_H
