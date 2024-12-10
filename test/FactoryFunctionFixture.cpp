27ec99f1890a6a76ebd6dd0dd1fbf55fd4ea4695
Luca Berto <luca.berto@edu.unifi.it>
2024-12-10 13:28:03 +0100

    Aggiunti test per FactoryFunction

--- test/FactoryFunctionFixture.cpp
+++ test/FactoryFunctionFixture.cpp
@@ -21,7 +21,15 @@ TEST_F(FactoryFunctionSuite, TestCreateFunction) {
     ASSERT_EQ(typeid(*function), typeid(Sum));
     delete function;
 
+    function = factory.createFunction(&sheet, "sum", range);
+    ASSERT_EQ(typeid(*function), typeid(Sum));
+    delete function;
+
     function = factory.createFunction(&sheet, "MAX", range);
+    ASSERT_EQ(typeid(*function), typeid(Max));
+    delete function;
+
+    function = factory.createFunction(&sheet, "max", range);
     ASSERT_EQ(typeid(*function), typeid(Max));
     delete function;
 
@@ -29,12 +37,29 @@ TEST_F(FactoryFunctionSuite, TestCreateFunction) {
     ASSERT_EQ(typeid(*function), typeid(Min));
     delete function;
 
+    function = factory.createFunction(&sheet, "min", range);
+    ASSERT_EQ(typeid(*function), typeid(Min));
+    delete function;
+
     function = factory.createFunction(&sheet, "MEAN", range);
+    ASSERT_EQ(typeid(*function), typeid(Mean));
+    delete function;
+
+    function = factory.createFunction(&sheet, "mean", range);
     ASSERT_EQ(typeid(*function), typeid(Mean));
     delete function;
 
     try {
         function = factory.createFunction(&sheet, "ALTRO", range);
+        FAIL() << "Nessuna eccezione lanciata";
+    } catch (std::invalid_argument &e) {
+        ASSERT_EQ(e.what(), std::string("Funzione non valida"));
+    } catch (...) {
+        FAIL() << "Lanciata altra eccesione";
+    }
+
+    try {
+        function = factory.createFunction(&sheet, "altro", range);
         FAIL() << "Nessuna eccezione lanciata";
     } catch (std::invalid_argument &e) {
         ASSERT_EQ(e.what(), std::string("Funzione non valida"));
