e300b64488b5631b14b2912b95921f3fdefa9fb1
Luca Berto <luca.berto@edu.unifi.it>
2024-12-14 16:05:57 +0100

    Creati Model.h e Model.cpp

---
+++ src/Model.cpp
@@ -0,0 +1,5 @@
+//
+// Created by lucaberto on 14/12/24.
+//
+
+#include "Model.h"
---
+++ src/Model.h
@@ -0,0 +1,11 @@
+//
+// Created by lucaberto on 14/12/24.
+//
+
+#ifndef MODEL_H
+#define MODEL_H
+
+class Model {
+};
+
+#endif //MODEL_H
