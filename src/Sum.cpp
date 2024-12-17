16cc527238e9c0bf5fa54f7c76f7b071734fc345
Luca Berto <luca.berto@edu.unifi.it>
2024-12-17 22:34:48 +0100

    Aggiunto parametro formale indexes sul costruttore

--- src/Sum.cpp
+++ src/Sum.cpp
@@ -4,7 +4,8 @@
 
 #include "Sum.h"
 
-Sum::Sum(Model *model, const QModelIndex &index, const std::string &formula) : Function(model, index, formula) {
+Sum::Sum(Model *model, const QModelIndex &index, const QModelIndexList &indexes, const std::string &formula) : Function(
+    model, index, indexes, formula) {
 }
 
 void Sum::compute() {
