ddb37f596ba81cb6fcd786572af005f4731be39c
Luca Berto <luca.berto@edu.unifi.it>
2024-12-17 13:35:08 +0100

    Lettura formula su funzione createFunction

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -20,17 +20,26 @@ void Controller::execute(const QModelIndex &index, const QString &newItemText) {
 
 void Controller::createFunction(const QModelIndex &index, const QString &newItemText) {
     FactoryFunction factory;
-    std::vector<std::string> splitFormula;
-
     std::string formula = newItemText.toStdString();
     std::transform(formula.begin(), formula.end(), formula.begin(), ::toupper);
+    std::string function;
+    char columnStart;
+    char columnEnd;
+    int rowStart;
+    int rowEnd;
+
+
+    std::regex pattern(R"(^=(SUM|MAX|MIN|MEAN)\(([A-J])(\d+):([A-J])(\d+)\)$)");
     std::smatch matches;
-    std::regex pattern("^=(SUM|MAX|MIN|MEAN)\\(([A-Z])(\\d+):([A-Z])(\\d+)\\)$");
-    while (std::regex_search(formula, matches, pattern)) {
-        for (auto match : matches) {
-            splitFormula.push_back(match);
-        }
-        formula = matches.suffix().str();
+    if (std::regex_match(formula, matches, pattern)) {
+        function = matches[1];
+        columnStart = matches[2].str()[0];
+        rowStart = std::stoi(matches[3].str());
+        columnEnd = matches[4].str()[0];
+        rowEnd = std::stoi(matches[5].str());
+    } else {
+        // TODO: individuare errore piu' appropriato
+        throw std::invalid_argument("La stringa non corrisponde al formato atteso");
     }
 
     // TODO: creare QModelIndexList che dovrà essere passato come parametro
