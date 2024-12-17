d4150d74db883f16daf7d0c547cbdcdf7ecb9a1e
Luca Berto <luca.berto@edu.unifi.it>
2024-12-17 23:45:06 +0100

    MainWindow test

---
+++ test/MainWindowTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by lucaberto on 17/12/24.
+//
+
+#include "gtest/gtest.h"
+#include "../src/MainWindow.h"
+
+TEST(MainWindow, TestCostructor) {
+    Model model(10, 10);
+    Controller controller(&model);
+    View view(&model, &controller);
+    MainWindow window(&view);
+
+    ASSERT_EQ(window.windowTitle(), "Spreadsheet");
+}
