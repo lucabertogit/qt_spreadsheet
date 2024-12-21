b72cb42769b23fac515f4b4af7e14b9818c6f7f0
Luca Berto <luca.berto@edu.unifi.it>
2024-12-21 23:47:15 +0100

    Main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <QtWidgets/QApplication>
-
-#include "src/Spreadsheet.h"
+#include "src/MainWindow.h"
+#include "src/View.h"
+#include "src/Model.h"
+#include "src/Controller.h"
 
 int main(int argc, char **argv) {
     QApplication a(argc, argv);
 
-    Spreadsheet sheet;
-    sheet.show();
+    Model model;
+    Controller controller(&model);
+    View view(&model, &controller);
+    MainWindow window(&view);
+
+    window.show();
 
     return a.exec();
 }
