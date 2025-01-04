9a1a1bb441153088e5aadf632398de183e787f5f
Luca Berto <luca.berto@edu.unifi.it>
2025-01-04 14:29:36 +0100

    Aggiunti QTest

--- test_qt/TestGui.cpp
+++ test_qt/TestGui.cpp
@@ -1,36 +1,89 @@
-#include <QtWidgets>
 #include <QTest>
+#include "../src/Model.h"
+#include "../src/Controller.h"
+#include "../src/View.h"
 
 class TestGui : public QObject {
     Q_OBJECT
 private slots:
-    void testGui_data();
     void testGui();
 };
 
-void TestGui::testGui_data() {
-    QTest::addColumn<QTestEventList>("events");
-    QTest::addColumn<QString>("expected");
-
-    QTestEventList list1;
-    list1.addKeyClick('a');
-    QTest::newRow("char") << list1 << "a";
-
-    QTestEventList list2;
-    list2.addKeyClick('a');
-    list2.addKeyClick(Qt::Key_Backspace);
-    QTest::newRow("there+back-again") << list2 << "";
-}
-
 void TestGui::testGui() {
-    QFETCH(QTestEventList, events);
-    QFETCH(QString, expected);
+    Model *model = new Model;
+    Controller *controller = new Controller(model);
+    View *view = new View(model, controller);
 
-    QLineEdit lineEdit;
+    // cella A1
+    QTest::mouseClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::mouseDClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::keyClicks(view->viewport()->focusWidget(), "23");
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport()->focusWidget(), Qt::Key_Enter);
+    QTest::qWait(100);
+    QCOMPARE(model->itemFromIndex(view->currentIndex())->text(), QString("23"));
 
-    events.simulate(&lineEdit);
+    // cella A2
+    QTest::keyClick(view->viewport(), Qt::Key_Down);
+    QTest::qWait(100);
+    QTest::mouseClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::mouseDClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::keyClicks(view->viewport()->focusWidget(), "74");
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport()->focusWidget(), Qt::Key_Enter);
+    QTest::qWait(100);
+    QCOMPARE(model->itemFromIndex(view->currentIndex())->text(), QString("74"));
 
-    QCOMPARE(lineEdit.text(), expected);
+    // cella B1
+    QTest::keyClick(view->viewport(), Qt::Key_Right);
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport(), Qt::Key_Up);
+    QTest::qWait(100);
+    QTest::mouseClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::mouseDClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::keyClicks(view->viewport()->focusWidget(), "65");
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport()->focusWidget(), Qt::Key_Enter);
+    QTest::qWait(100);
+    QCOMPARE(model->itemFromIndex(view->currentIndex())->text(), QString("65"));
+
+    // cella B2
+    QTest::keyClick(view->viewport(), Qt::Key_Down);
+    QTest::qWait(100);
+    QTest::mouseClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::mouseDClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::keyClicks(view->viewport()->focusWidget(), "24");
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport()->focusWidget(), Qt::Key_Enter);
+    QTest::qWait(100);
+    QCOMPARE(model->itemFromIndex(view->currentIndex())->text(), QString("24"));
+
+    // cella A3
+    QTest::keyClick(view->viewport(), Qt::Key_Down);
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport(), Qt::Key_Left);
+    QTest::qWait(100);
+    QTest::mouseClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::mouseDClick(view->viewport(), Qt::LeftButton, Qt::NoModifier, view->visualRect(view->currentIndex()).center());
+    QTest::qWait(100);
+    QTest::keyClicks(view->viewport()->focusWidget(), "=MAX(A1:B2)");
+    QTest::qWait(100);
+    QTest::keyClick(view->viewport()->focusWidget(), Qt::Key_Enter);
+    QTest::qWait(100);
+    QCOMPARE(model->itemFromIndex(view->currentIndex())->text(), QString("74"));
+
+    delete view;
+    delete controller;
+    delete model;
 }
 
 QTEST_MAIN(TestGui)
